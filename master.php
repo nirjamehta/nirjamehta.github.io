46e6b19e0fc6cb905e8d3b1dd11baf22363f7783
nirjamehta <49746479+nirjamehta@users.noreply.github.com>
2019-04-18 14:42:10 +0530

    Create master.php

---
+++ master.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+ <head>
+<title>GPSEA Portal</title>
+ </head>
+
+<body>
+ <div style="position:absolute; left:-500px; top:30px;" "background-color:lightblue;">
+  <form>
+   <a href="LogIn.html">Log In</a>
+   <a href="SignUp.html">Sign Up</a>
+   <a href="AboutUs.html">About Us</a>
+   <a href="ContactUs.html">Contact Us</a>
+  </form>
+</div>
+</body>
+</html>
